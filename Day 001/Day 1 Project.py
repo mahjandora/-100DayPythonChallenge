fb586c712bce63322588e1f55b0e07b6d412a3c0
Mah Jan Dorazahi <mahieg.joie@gmail.com>
2022-05-01 11:29:36 +0200

    Day001

--- Day 001/Day 1 Project.py
+++ Day 001/Day 1 Project.py
@@ -1,5 +1,4 @@
-city_name = input("What's name of the city you grew up in? \\ ")
-print(city_name)
-pet_name = input("What's your pet's name? \\ ")
-print(pet_name)
+print("Welcome to the Band Name Generator. ")
+city_name = input("What's name of the city you grew up in? \n ")
+pet_name = input("What's your pet's name? \n ")
 print("Your band name could be "+ city_name +" "+ pet_name)
